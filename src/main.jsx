f14021e4007b60d7a251fcc11066d16c19b1a8fd
ardabarinhatipoglu <168469305+ardabarinhatipoglu@users.noreply.github.com>
2026-08-25 10:42:11 +0300

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ClimateMap from "./ClimateMap.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ClimateMap />
+  </React.StrictMode>
+);
